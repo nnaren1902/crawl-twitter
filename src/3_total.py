73f9d08459a1cebae43eaae007973c4c3945c455
nnaren1902 <nnaren1902@gmail.com>
2015-02-18 14:23:02 -0800

    plot 2 of part 3 left

--- src/3_total.py
+++ src/3_total.py
@@ -1,8 +1,40 @@
-
+import config as config
+import common as common
 
 total_count = {}
+##first part
 with open("../output/count.txt") as f:
     for line in f:
         (key, val) = line.split("\t")
         total_count[str(key)] = int(val)
        
+       
+###for the second plot
+
+popular_hashTag = ['#SuperBowlXLIX']
+
+for hashTag in popular_hashTag:
+    
+    startTime = config.mintime
+    endTime = config.maxtime
+
+    timeDelta = 10
+    winMinTime = startTime
+     
+    while(winMinTime <= endTime):
+        config.params = config.createParameters(config.API_KEY, hashTag, winMinTime, winMinTime+timeDelta, config.new_only_bool, config.include_metrics_bool, 499)
+        
+        #Send Request
+        request = config.makeRequest(config.url, config.params)
+        response = request.getresponse()
+
+        tweets = common.extractTweetsFromResponse(response)
+        numberOfTweetsReceived = len(tweets)
+        
+        hashTag_rate = numberOfTweetsReceived/10
+        common.logRateResults(hashTag, hashTag_rate, winMinTime, winMinTime+timeDelta, "../output/rate_log.txt")
+        winMinTime = winMinTime + timeDelta
+           
+       
+
+
--- src/common.py
+++ src/common.py
@@ -27,6 +27,7 @@ def writeResponseAsTweetsToFile(response,tweets, outputFile,mode='w+'):
         
     output_file.write(json_string_for_output);
     print("Done!")
+    output_file.close()
     
 def logSearchResults(hashtag, numberOfTweets,unixStartTime,unixEndTime,outputFile,mode):
     startTime = datetime.datetime.fromtimestamp(unixStartTime).strftime('%Y-%m-%d %H:%M:%S')
@@ -40,3 +41,20 @@ def logSearchResults(hashtag, numberOfTweets,unixStartTime,unixEndTime,outputFil
     output_string = "Hashtag: "+hashtag+" \tfrom: "+str(startTime)+" \tto: "+str(endTime)+" \tNo. of Results: "+str(numberOfTweets)+"\n"        
     output_file.write(output_string);
     print("Done!")
+    output_file.close()
+    
+    
+def logRateResults(hashTag, hashTag_rate, winMinTime,winMaxTime,outputFile):
+    startTime = datetime.datetime.fromtimestamp(winMinTime).strftime('%H:%M:%S')
+    endTime = datetime.datetime.fromtimestamp(winMaxTime).strftime('%H:%M:%S')
+    
+    print("Logging popular hashTag rate to file...") 
+    
+    output_file = open(outputFile,'a')
+    output_string = "Hashtag: "+hashTag+" \tfrom: "+str(startTime)+" \tto: "+str(endTime)+" \tHashTag_rate: "+str(hashTag_rate)+"\n"
+    output_file.write(output_string)
+    print("Done")
+    output_file.close()
+    
+    
+    
